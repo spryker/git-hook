c252e791c4c0c2027290f758cd349171a1c5faca
maslov <dev.maslov.a@gmail.com>
2020-12-07 16:30:24 +0200

    TE-7403: Implemented precomit hook for prettier and stylelint

---
+++ src/GitHook/Command/FileCommand/PreCommit/FormatterCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * MIT License
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace GitHook\Command\FileCommand\PreCommit;
+
+use GitHook\Command\CommandConfigurationInterface;
+use GitHook\Command\CommandInterface;
+use GitHook\Command\CommandResult;
+use GitHook\Command\CommandResultInterface;
+use GitHook\Command\Context\CommandContextInterface;
+use GitHook\Helper\ProcessBuilderHelper;
+
+class FormatterCommand implements CommandInterface
+{
+    use ProcessBuilderHelper;
+
+    /**
+     * @param \GitHook\Command\CommandConfigurationInterface $commandConfiguration
+     *
+     * @return \GitHook\Command\CommandConfigurationInterface
+     */
+    public function configure(CommandConfigurationInterface $commandConfiguration): CommandConfigurationInterface
+    {
+        $commandConfiguration
+            ->setName('Formatter')
+            ->setDescription('Formated files according to prettier rules.')
+            ->setAcceptedFileExtensions(['scss', 'css', 'less', 'js', 'ts', 'json', 'html']);
+
+        return $commandConfiguration;
+    }
+
+    /**
+     * @param \GitHook\Command\Context\CommandContextInterface $context
+     *
+     * @return \GitHook\Command\CommandResultInterface
+     */
+    public function run(CommandContextInterface $context): CommandResultInterface
+    {
+        $commandResult = new CommandResult();
+
+        $processDefinition = ['npx', 'prettier', '--config', 'node_modules/@spryker/frontend-config.prettier/.prettierrc.json', '--write', $context->getFile()];
+        $process = $this->buildProcess($processDefinition);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            $commandResult
+                ->setError(trim($process->getErrorOutput()))
+                ->setMessage(trim($process->getOutput()));
+        }
+
+        return $commandResult;
+    }
+}
