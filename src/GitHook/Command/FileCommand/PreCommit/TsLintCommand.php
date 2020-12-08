4278239c8977acc73eaa40baba8b41ceb9d4554e
maslov <dev.maslov.a@gmail.com>
2020-12-08 15:43:37 +0200

    TE-7403: Implemented precomit hook for tslint

---
+++ src/GitHook/Command/FileCommand/PreCommit/TsLintCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * MIT License
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace GitHook\Command\FileCommand\PreCommit;
+
+use GitHook\Command\CommandConfigurationInterface;
+use GitHook\Command\CommandInterface;
+use GitHook\Command\CommandResult;
+use GitHook\Command\CommandResultInterface;
+use GitHook\Command\Context\CommandContextInterface;
+use GitHook\Helper\ProcessBuilderHelper;
+
+class TsLintCommand implements CommandInterface
+{
+    use ProcessBuilderHelper;
+
+    /**
+     * @param \GitHook\Command\CommandConfigurationInterface $commandConfiguration
+     *
+     * @return \GitHook\Command\CommandConfigurationInterface
+     */
+    public function configure(CommandConfigurationInterface $commandConfiguration): CommandConfigurationInterface
+    {
+        $commandConfiguration
+            ->setName('TsLint')
+            ->setDescription('Fix ts files according to typescript rules.')
+            ->setAcceptedFileExtensions('ts');
+
+        return $commandConfiguration;
+    }
+
+    /**
+     * @param \GitHook\Command\Context\CommandContextInterface $context
+     *
+     * @return \GitHook\Command\CommandResultInterface
+     */
+    public function run(CommandContextInterface $context): CommandResultInterface
+    {
+        $commandResult = new CommandResult();
+
+        $processDefinition = ['node', './frontend/libs/tslint', 'fix', $context->getFile()];
+        $process = $this->buildProcess($processDefinition);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            $commandResult
+                ->setError(trim($process->getErrorOutput()))
+                ->setMessage(trim($process->getOutput()));
+        }
+
+        return $commandResult;
+    }
+}
