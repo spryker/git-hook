1b0452088d98d34ddca62a930901b0f7fcc75066
Yaroslav Krutikov <yaroslav.krutikov@spryker.com>
2022-10-06 12:44:34 +0200

    FRW-390 Added descriptive error message

--- src/GitHook/Command/FileCommand/PreCommit/CodeStyleFixCommand.php
+++ src/GitHook/Command/FileCommand/PreCommit/CodeStyleFixCommand.php
@@ -13,6 +13,7 @@ use GitHook\Command\CommandResult;
 use GitHook\Command\CommandResultInterface;
 use GitHook\Command\Context\CommandContextInterface;
 use GitHook\Helper\ProcessBuilderHelper;
+use Symfony\Component\Process\Process;
 
 class CodeStyleFixCommand implements CommandInterface
 {
@@ -22,6 +23,16 @@ class CodeStyleFixCommand implements CommandInterface
      * @var string
      */
     protected const BINARY_STYLE_FIXER = 'vendor/bin/phpcbf';
+
+    /**
+     * @var int
+     */
+    protected const EXIT_CODE_SUCCESS = 0;
+
+    /**
+     * @var string
+     */
+    protected const ERROR_FILE_DOES_NOT_EXIST = 'Configuration file does not exist';
 
     /**
      * @var string
@@ -60,30 +71,72 @@ class CodeStyleFixCommand implements CommandInterface
      */
     public function run(CommandContextInterface $context): CommandResultInterface
     {
-        $commandResult = new CommandResult();
-
         $processDefinition = [
             static::BINARY_STYLE_FIXER,
             $context->getFile(),
-            $this->getStandardRulesetOption(),
         ];
+
+        $standardOption = $this->getStandardRulesetOption();
+        if ($standardOption !== null) {
+            $processDefinition[] = $standardOption;
+        }
 
         $process = $this->buildProcess($processDefinition);
         $process->run();
+
+        return $this->createCommandResult($process);
+    }
+
+    /**
+     * @param \Symfony\Component\Process\Process $process
+     *
+     * @return \GitHook\Command\CommandResultInterface
+     */
+    protected function createCommandResult(Process $process): CommandResultInterface
+    {
+        $commandResult = new CommandResult();
+
+        if ($process->getExitCode() === static::EXIT_CODE_SUCCESS) {
+            return $commandResult;
+        }
+
+        $errorMessage = $this->getErrorMessage($process);
+
+        $commandResult
+            ->setError($errorMessage)
+            ->setMessage($process->getOutput());
 
         return $commandResult;
     }
 
     /**
+     * @param \Symfony\Component\Process\Process $process
+     *
      * @return string
      */
-    protected function getStandardRulesetOption(): string
+    protected function getErrorMessage(Process $process): string
     {
-        $filepath = static::RULESET_LEGACY_PATH;
-        if (file_exists(PROJECT_ROOT . DIRECTORY_SEPARATOR . static::RULESET_PATH)) {
-            $filepath = static::RULESET_PATH;
+        $errorMessage = $process->getOutput();
+        if (strpos($errorMessage, static::RULESET_LEGACY_PATH) !== null) {
+            $errorMessage = sprintf(
+                '%s: %s',
+                static::ERROR_FILE_DOES_NOT_EXIST,
+                static::RULESET_LEGACY_PATH,
+            );
         }
 
-        return sprintf('%s=%s', static::OPTION_STANDARD, $filepath);
+        return $errorMessage;
+    }
+
+    /**
+     * @return string|null
+     */
+    protected function getStandardRulesetOption(): ?string
+    {
+        if (file_exists(PROJECT_ROOT . DIRECTORY_SEPARATOR . static::RULESET_PATH)) {
+            return null;
+        }
+
+        return sprintf('%s=%s', static::OPTION_STANDARD, static::RULESET_LEGACY_PATH);
     }
 }
