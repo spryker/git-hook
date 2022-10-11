d2a0db37f6e107f8798c38dd4421c8a265046f1c
Yaroslav Krutikov <yaroslav.krutikov@spryker.com>
2022-10-11 11:10:16 +0200

    FRW-390 Added usage of new ruleset filepath and fallback to legacy (#63)
    * FRW-390 Added usage of new ruleset filepath and fallback to legacy
    
    * FRW-390 Added descriptive error message
    
    * FRW-390 Added more meaning to error message
    
    * Update ci.yml
    
    Co-authored-by: Mark Scherer <dereuromark@users.noreply.github.com>

--- src/GitHook/Command/FileCommand/PreCommit/CodeStyleFixCommand.php
+++ src/GitHook/Command/FileCommand/PreCommit/CodeStyleFixCommand.php
@@ -13,10 +13,41 @@ use GitHook\Command\CommandResult;
 use GitHook\Command\CommandResultInterface;
 use GitHook\Command\Context\CommandContextInterface;
 use GitHook\Helper\ProcessBuilderHelper;
+use Symfony\Component\Process\Process;
 
 class CodeStyleFixCommand implements CommandInterface
 {
     use ProcessBuilderHelper;
+
+    /**
+     * @var string
+     */
+    protected const BINARY_STYLE_FIXER = 'vendor/bin/phpcbf';
+
+    /**
+     * @var int
+     */
+    protected const EXIT_CODE_SUCCESS = 0;
+
+    /**
+     * @var string
+     */
+    protected const ERROR_FILE_DOES_NOT_EXIST = 'Please add root configuration file. Configuration file does not exist';
+
+    /**
+     * @var string
+     */
+    protected const OPTION_STANDARD = '--standard';
+
+    /**
+     * @var string
+     */
+    protected const RULESET_PATH = 'phpcs.xml';
+
+    /**
+     * @var string
+     */
+    protected const RULESET_LEGACY_PATH = 'config/ruleset.xml';
 
     /**
      * @param \GitHook\Command\CommandConfigurationInterface $commandConfiguration
@@ -40,12 +71,72 @@ class CodeStyleFixCommand implements CommandInterface
      */
     public function run(CommandContextInterface $context): CommandResultInterface
     {
-        $commandResult = new CommandResult();
+        $processDefinition = [
+            static::BINARY_STYLE_FIXER,
+            $context->getFile(),
+        ];
 
-        $processDefinition = ['vendor/bin/phpcbf', $context->getFile(), '--standard=config/ruleset.xml'];
+        $standardOption = $this->getStandardRulesetOption();
+        if ($standardOption !== null) {
+            $processDefinition[] = $standardOption;
+        }
+
         $process = $this->buildProcess($processDefinition);
         $process->run();
 
+        return $this->createCommandResult($process);
+    }
+
+    /**
+     * @param \Symfony\Component\Process\Process $process
+     *
+     * @return \GitHook\Command\CommandResultInterface
+     */
+    protected function createCommandResult(Process $process): CommandResultInterface
+    {
+        $commandResult = new CommandResult();
+
+        if ($process->getExitCode() === static::EXIT_CODE_SUCCESS) {
+            return $commandResult;
+        }
+
+        $errorMessage = $this->getErrorMessage($process);
+
+        $commandResult
+            ->setError($errorMessage)
+            ->setMessage($process->getOutput());
+
         return $commandResult;
+    }
+
+    /**
+     * @param \Symfony\Component\Process\Process $process
+     *
+     * @return string
+     */
+    protected function getErrorMessage(Process $process): string
+    {
+        $errorMessage = $process->getOutput();
+        if (strpos($errorMessage, static::RULESET_LEGACY_PATH) !== null) {
+            $errorMessage = sprintf(
+                '%s: %s',
+                static::ERROR_FILE_DOES_NOT_EXIST,
+                static::RULESET_LEGACY_PATH,
+            );
+        }
+
+        return $errorMessage;
+    }
+
+    /**
+     * @return string|null
+     */
+    protected function getStandardRulesetOption(): ?string
+    {
+        if (file_exists(PROJECT_ROOT . DIRECTORY_SEPARATOR . static::RULESET_PATH)) {
+            return null;
+        }
+
+        return sprintf('%s=%s', static::OPTION_STANDARD, static::RULESET_LEGACY_PATH);
     }
 }
