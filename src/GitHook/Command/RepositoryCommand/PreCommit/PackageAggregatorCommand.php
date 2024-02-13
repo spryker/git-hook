add51039bca5c8efbeb80e1b38b41a56b1266aca
Karoly Gerner <karoly.gerner@spryker.com>
2024-02-13 13:21:36 +0100

    AE-57 Moving command from file level to repository level

--- src/GitHook/Command/RepositoryCommand/PreCommit/PackageAggregatorCommand.php
+++ src/GitHook/Command/RepositoryCommand/PreCommit/PackageAggregatorCommand.php
@@ -1,11 +1,6 @@
 <?php
 
-/**
- * MIT License
- * For full license information, please view the LICENSE file that was distributed with this source code.
- */
-
-namespace GitHook\Command\FileCommand\PreCommit;
+namespace GitHook\Command\RepositoryCommand\PreCommit;
 
 use GitHook\Command\CommandConfigurationInterface;
 use GitHook\Command\CommandInterface;
@@ -13,17 +8,7 @@ use GitHook\Command\CommandResult;
 use GitHook\Command\CommandResultInterface;
 use GitHook\Command\Context\CommandContextInterface;
 use GitHook\Helper\ProcessBuilderHelper;
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
 
-/**
- * The script traverses through the project directory structure, collecting dependencies and devDependencies
- * from all package.json files, excluding those within node_modules directories.
- * The gathered dependencies are stored in the package-aggregated.txt file.
- *
- * The compare mode conducts a comparison between the generated dependency list (without saving)
- * and the existing package-aggregated.txt file. Any difference between the lists result in an error.
- */
 class PackageAggregatorCommand implements CommandInterface
 {
     use ProcessBuilderHelper;
@@ -37,8 +22,7 @@ class PackageAggregatorCommand implements CommandInterface
     {
         $commandConfiguration
             ->setName('Package Aggregator')
-            ->setDescription('Aggregates package.json dependencies across the project.')
-            ->setAcceptedFileExtensions('php');
+            ->setDescription('Aggregates package.json dependencies across the project.');
 
         return $commandConfiguration;
     }
@@ -50,16 +34,11 @@ class PackageAggregatorCommand implements CommandInterface
      */
     public function run(CommandContextInterface $context): CommandResultInterface
     {
-        $commandResult = new CommandResult();
-
         $directory = $context->getProjectPath();
 
-        $processDefinition = [
-            'vendor/bin/package-aggregator',
-            $directory,
-        ];
+        $commandResult = new CommandResult();
 
-        $process = $this->buildProcess($processDefinition);
+        $process = $this->buildProcess(['vendor/bin/package-aggregator', $directory, '--compare'], PROJECT_ROOT . PATH_PREFIX);
         $process->run();
 
         if (!$process->isSuccessful()) {
